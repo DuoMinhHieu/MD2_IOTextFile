4303221cbba5b8d36ce87b37cfbd84de3531df02
DuoMinhHieu <dmh6774@gmail.com>
2024-03-26 16:17:25 +0700

    main

--- demo/src/Main.java
+++ demo/src/Main.java
@@ -1,5 +1,18 @@
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        InputStream is = System.in;
+        while (true) {
+            System.out.print("Nhập 1 ký tự: ");
+            int ch = is.read();
+            if (ch == 'q') {
+                System.out.println("Finished!");
+                break;
+            }
+            is.skip(2);
+            System.out.println("Ký tự nhận được: " + (char) ch);
+        }
     }
 }
